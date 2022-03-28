51b489ba8e65c9c43bdab3763dae2ee1df8ddf0e
Ahmed Elkfafy <73217710+darkerfreelancer@users.noreply.github.com>
2022-03-28 19:16:50 +0200

    added

--- back-end/server.js
+++ back-end/server.js
@@ -3,6 +3,54 @@ const app = express();
 const bodyParser = require('body-Parser');
 
 
+//* Mongoose contactor with mongo db data base
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/users', 
+{
+    autoIndex: false,
+
+})
+.then(() => console.log('connected successfully'))
+.catch((err) => console.log(err));
+//
+
+// USERS SCHEMA //
+const userschema = new mongoose.Schema({
+    username: {
+        type: String,
+        unique: true
+    },
+    email: {
+        type: String,
+        unique: true,
+
+    },
+    password: String
+});
+//
+
+// modeling users { making user object }
+const user = mongoose.model('user', userschema);
+//
+
+/*
+const goodUser = {
+    username: 'all',
+    email: 'all.adel@gmail.com',
+    password: 'tryAgain'
+};
+
+user.init()
+.then(() => user.create(goodUser, (err) => console.log(err)))
+.catch((err) => console.log(err));
+*/
+//goodUser.save().catch((err) => console.log('errrr finally'));
+
+user.find()
+.then((data) => console.log(data))
+.catch((err) => console.log(err));
+
+
   //**************************************************
   const Ajv=require("ajv");
   const ajv = new Ajv();
@@ -50,16 +98,23 @@ app.get('/api', (req, res) => {
 
 
 app.post('/api/login', (req, res) => {
-    const checker = {
-        username: 'ahmed',
-        password: 'ahmed'
-    };
-  
     console.log(req.body);
-    if (req.body.username == checker.username && req.body.password == checker.password)
-        res.send(true);
-    else
-        res.send(false);
+    user.find({username: req.body.username, password: req.body.password})
+    .then((data) => {
+        if (data.length !== 0)
+        {
+            res.send(true);
+        }
+        else
+        {
+            console.log(data);
+            res.send(false);
+        }
+    })
+    .catch((err) =>
+    {
+        console.log(err);
+    });
 });
   //********************************************
 
@@ -67,10 +122,14 @@ app.post("/api/register",(req,res)=>{
     console.log(req.body);
     let valid = validator(req.body);
     if (valid){
-        users.push(req.body);      //need to be stored in db
+        //users.push(req.body);      //need to be stored in db
+        user.init()
+        .then(() => user.create(req.body).then(() => res.send(true)).catch((err) => {console.log(err);res.send(false)}));
     }
+    else
+        res.send(valid);
+
     console.log(valid);
-    res.send(valid);
 
 
 });
