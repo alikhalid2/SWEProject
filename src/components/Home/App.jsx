5def7fc0b59f5fc203b88671054b5a03cfcc39e5
EngYoussef1 <101152968+EngYoussef1@users.noreply.github.com>
2022-04-12 20:34:32 +0200

    new3

--- src/components/Home/App.jsx
+++ src/components/Home/App.jsx
@@ -20,12 +20,12 @@ export default class App extends Component{
 			<div className="bar2">
 	<nav id="navbar">
 	<ul className="navul">
-	<Link to = '/ahmed'><li  className="navli"> All Game </li></Link>
-	<li className="navli"> Latest Game </li>
-	<li className="navli">Coming Soon </li>
-	<li className="navli">Star War </li>
-	<li className="navli">Star War </li>
-	<li className="navli">EA Sports </li>
+	<Link to = '/ahmed' style={{ textDecoration: 'none',color: 'white'  }}><li  className="navli"> All Game </li></Link>
+	<Link to = '/ahmed' style={{ textDecoration: 'none',color: 'white'  }}><li className="navli"> Latest Game </li></Link>
+	<Link to = '/ahmed' style={{ textDecoration: 'none',color: 'white'  }}><li className="navli">Coming Soon </li></Link>
+	<Link to = '/ahmed' style={{ textDecoration: 'none',color: 'white'  }}><li className="navli">Star War </li></Link>
+	<Link to = '/ahmed' style={{ textDecoration: 'none',color: 'white'  }}><li className="navli">Star War </li></Link>
+	<Link to = '/ahmed' style={{ textDecoration: 'none',color: 'white'  }}><li className="navli">EA Sports </li></Link>
 	
 	</ul>
 
